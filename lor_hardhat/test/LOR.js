e637670288f027ec91e7b18e8d1adaacd1175637
mandar-saiyan87 <mdman2257@gmail.com>
2025-08-06 17:49:24 +0530

    test owner can change approver - done

--- lor_hardhat/test/LOR.js
+++ lor_hardhat/test/LOR.js
@@ -7,7 +7,7 @@ describe("LOR", function () {
 
     async function deployLORFixture() {
 
-        const [owner, student] = await hre.ethers.getSigners()
+        const [owner, approver, student] = await hre.ethers.getSigners()
         const lorContractFactory = await hre.ethers.getContractFactory("LOR")
         const LOR = await lorContractFactory.deploy()
         await LOR.waitForDeployment()
@@ -16,7 +16,7 @@ describe("LOR", function () {
         console.log(`Owner: ${owner.address}`)
         console.log(`Approver: ${owner.address}`)
         console.log(`Student: ${student.address}`)
-        return { LOR, owner, student }
+        return { LOR, owner, student, approver }
     }
 
 
@@ -70,4 +70,26 @@ describe("LOR", function () {
 
         await expect(approve).to.emit(LOR, "LORApproved").withArgs(requestId, owner, studentAddress)
     })
+    it("Only owner can change approver", async function () {
+        const { LOR, owner, approver, student } = await loadFixture(deployLORFixture)
+        await LOR.updateApprover(approver.address)
+
+        console.log(`Owner: ${owner.address}`)
+        console.log(`Approver: ${approver.address}`)
+
+        const requestId = 1001
+        const name = "Mandar Deshpande"
+        const program = "Blockchain Development"
+        const university = "XYZ University"
+        const studentAddress = student.address
+
+        const tx = await LOR.connect(student).createLORRequest(requestId, studentAddress, name, program, university)
+
+        await tx.wait()
+
+        const approve = await LOR.connect(approver).approveLORRequest(requestId)
+        await expect(approve).to.emit(LOR, "LORApproved").withArgs(requestId, approver.address, studentAddress)
+
+    })
+
 })
