8b9ea68d85b88e397e3b9de2a159d1cfe4f66215
mandar-saiyan87 <mdman2257@gmail.com>
2025-09-19 12:06:03 +0530

    module not found error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
-import { LOREventListerner } from "../services/LOREventListerner";
+import { LOREventListerner } from "../services/LOREventListerner.js";
 
 config()
 
